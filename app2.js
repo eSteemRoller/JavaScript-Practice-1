465cb75a27905ca8cc6c5686c9defffcbd2547b1
Jason <jasondoroughonfb@live.com>
2025-03-19 21:29:55 -0700

    Third commit

---
+++ app2.js
@@ -0,0 +1,7 @@
+
+function changeTitleToRed() {
+    document.querySelector(`#title`).style.color = `red`;
+    console.log(`clicked`);
+  }
+  
+  console.log(document.querySelector(`#title`).style.color);
