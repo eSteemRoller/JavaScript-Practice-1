a956105bbd1e895d785c1a81ac11ef285c93af24
Jason <jasondoroughonfb@live.com>
2025-03-21 03:15:47 -0700

    Fourth commit

--- app.js
+++ app.js
@@ -21,7 +21,140 @@
 // If the value of the expression following the await operator is not a Promise, it's converted to a resolved Promise.
 
 
-fetch(`https://jsonplaceholder.typicode.com/users/1`).then(() => {
+// const emailRef = document.querySelector(`.email`);
+// console.log(emailRef);
 
-  });
+// // The ".then()" Promise method (long syntax)
+// fetch(`https://jsonplaceholder.typicode.com/users/1`).then(response1a => {
+//   response1a.json().then(userDetails => {
+//   // console.log(userDetails);
+//   emailRef.innerHTML = userDetails.email;
+//   });
+// });
 
+// // The ".then()" Promise method (short syntax)
+// fetch(`https://jsonplaceholder.typicode.com/users/1`).then(response1b => {
+//   return response1b.json();
+// })
+// .then(userDetails => {
+//   console.log(userDetails);
+// });
+
+// // The "async/await" Promise method
+// async function main() {
+//   const response = await fetch(`https://jsonplaceholder.typicode.com/users/1`)
+//   const data = await response.json();
+//   console.log(data);
+//   emailRef.innerHTML = data.email;
+// }
+
+// main();
+
+// // The "async/await" Promise method with error handling
+// async function main() {
+//   try {
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/users/1`);
+//     const data = await response.json()
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+//   }
+//     catch (error) {
+//     console.error(`Error: ${error}`);
+//   }
+// }
+
+// main();
+
+// // The "async/await" Promise method with error handling and finally block
+// async function main() {
+//   try {
+//     const response = await fetch(`https://jsonplaceholder.typicode.com/users/1`);
+//     const data = await response.json()
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+//   }
+//   catch (error) {
+//     console.error(`Error: ${error}`);
+//   }
+//   finally {
+//     console.log(`Finally block executed.`);
+//   }
+// }
+
+// main();
+
+// const emailRef = document.querySelector(`.email`);
+
+// Create a promise
+
+// function getSubscriptionStatus() {
+//     return new Promise((resolve, reject) => {
+//         resolve(`VIP`);
+//     });
+// }
+
+// // 2 ways to console.log the promise data (already resolved; already front-loaded)
+
+// // 1. Using the `async/await` method
+// async function main() {
+// console.log(await getSubscriptionStatus());
+// }
+
+// // 2. Using the `.then()` method
+// getSubscriptionStatus().then(response => console.log(response));
+
+// main();
+
+
+// Using the final resolved promise data with HTML
+// (Mock API for Exercise 1)
+
+const statusRef = document.querySelector(`.status`);
+
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+    resolve(`VIP`);
+    });
+}
+
+// // 1. Using the `async/await` method
+// async function main() {
+//     // console.log(await getSubscriptionStatus());
+//     const subStatus = (await getSubscriptionStatus());
+//     subStatusRef.innerHTML = subStatus;
+// }
+
+// // 2. Using the `.then()` method
+// getSubscriptionStatus().then(response => console.log(response));
+
+// main();
+
+// Promise (Exercise 1)
+
+function getVideo(subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === `VIP`) {
+            resolve(`show video`);
+        }
+        else if (subscriptionStatus === `FREE`) {
+            resolve(`show trailer`);
+        }
+        else {
+            reject(`no video`);
+        }
+    });
+}
+
+async function main() {
+    const status = await getSubscriptionStatus();
+    statusRef.innerHTML = status;
+    try {
+        console.log(await getVideo(status));
+    }
+    catch (error) {
+        console.error(`Error: ${error}`);
+        console.log(`Please, sign in.`);
+    }
+}
+
+main();
